c110a4b5005c50a5fc3c233c2ac9b1ba4ca2622b
anecholls <anadelavegagonzalez@gmail.com>
2022-11-24 11:27:17 +0100

    update list

--- script.js
+++ script.js
@@ -1,38 +1,15 @@
-/* function insertarNombre(EventTarget){
+let nombres = []; /* Arreglo para uso global con los nombres a medida que se ingresan */
+
+function insertarNombre(EventTarget) {
+
     EventTarget.preventDefault();
 
     let nombre = document.getElementById("nombre").value;
+    nombres.push(nombre); 
+   
+    const lista = document.getElementById("lista-nombres");
+    lista.innerHTML += `<li> ${nombre}</li>`;
 
-    let opcion = "<li>" + nombre + "</li>";
-
-    let lista = document.getElementById("lista-nombres");
-
-    lista.InnerHTML += opcion;
-
-    alert("Sacrificado añadido");
+    /*alert("Sacrificado " +nombre+ " añadido");*/
 };
 
-function recogerDatos(){
-
-}
-
-*/
-
-
-
-let addNamesButton = document.getElementById("addNames");
-let namesContainer = document.getElementById ("namesContainer");
-let inpuField = document.getElementById("inputField");
-
-addToDoButton.addEventListener ("click", function(){
-    let paragraph =document.createElement("p");
-    paragraph.classList.add("paragraph-styling");
-    paragraph.innerText = inputField.value;
-    namesContainer.appendChild(paragraph);
-    inputField.value="";
-    paragraph.addEventListener("click", function(){
-        paragraph.style.textDecoration = "line-though";
-    })
-    paragraph.addEventListener("dblclick", function(){
-        namesContainer.appendChild(paragraph);
-})
